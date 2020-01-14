aab1989ea5ebdb8000b629b96cb9bc54a702962f
elzlociako <szymonsobczak2282@gmail.com>
2020-01-14 10:47:02 +0100

    zmianka

--- colaborative_script.py
+++ colaborative_script.py
@@ -1,5 +1,7 @@
 x = "This is my first python repository"
 print(x)
 for s in x:
-    print(s)
-    print("ASCII:",ord(s))
+    print(s,"-" ,ord(s))
+
+
+print('This changes were made by Szymon Sobczak.')
